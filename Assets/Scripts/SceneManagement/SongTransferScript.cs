be9d7cc15c99f196a4dd4b70f2bfe1bc9c2f752d
johhaanndev <71643284+johhaanndev@users.noreply.github.com>
2022-03-07 16:13:40 +0100

    Añadidas las canciones a los botones y función de jugar a los botones

--- Assets/Scripts/SceneManagement/SongTransferScript.cs
+++ Assets/Scripts/SceneManagement/SongTransferScript.cs
@@ -1,11 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SongTransferScript : MonoBehaviour
 {
     public string songName;
     public AudioClip trackSelected;
+
+    private void Start()
+    {
+        if (SceneManager.GetActiveScene().name == "SongList")
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
 
     public void LoadAudioSource(string songName)
     {
